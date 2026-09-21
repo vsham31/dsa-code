1841f5ab29fdda5dcdfb469a6192ed038d4fb91b
Vaibhav Sharma <vsham31@gmail.com>
2026-09-21 12:43:20 +0530

    remove duplicates from arr in cpp optimal added

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // nomal approach
-        set<int> st;
+        // // nomal approach
+        // set<int> st;
 
-        for(int i=0; i<nums.size(); i++){
-            st.insert(nums[i]);
+        // for(int i=0; i<nums.size(); i++){
+        //     st.insert(nums[i]);
+        // }
+
+        // int index=0;
+        // for(auto i: st){
+        //     nums[index]=i;
+        //     index++;
+        // }
+
+        // return st.size();
+
+        // optimal
+
+        int i=0;
+
+        for(int j=1; j<nums.size(); j++){
+            if(nums[i]!=nums[j]){
+                nums[i+1]=nums[j];
+                i++;
+            }
         }
-
-        int index=0;
-        for(auto i: st){
-            nums[index]=i;
-            index++;
-        }
-
-        return st.size();
+        return i+1;
     }
 };
